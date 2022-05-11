e381ccd5f819e0f0cd23a902ae37f6a43f54cffd
AhmerHussain07 <99546831+AhmerHussain07@users.noreply.github.com>
2022-05-11 21:56:19 +0500

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-import  pandas as pd
-import numpy as np
-testDF = pd.read_csv('test.csv')
-idDF = testDF[['id']]
-idDF.insert(1,"target",0)
-idDF['target'] = np.random.rand(700000,1)
-print(idDF)
-idDF.to_csv('out.CSV', index=False)
